dd0ca473142778e0f98c9f5f4e3d1d2c54e2dbc1
moffire <alexeydudarev.spb@gmail.com>
2021-03-23 15:40:09 +0300

    added comparing lists of groups after changes

--- addressbook-we-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-we-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,14 +1,50 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupData {
+    private final String id;
     private final String groupName;
     private final String formHeader;
     private final String formFooter;
 
-    public GroupData(String groupName, String formHeader, String formFooter) {
+    public GroupData(String id, String groupName, String formHeader, String formFooter) {
+        this.id = id;
         this.groupName = groupName;
         this.formHeader = formHeader;
         this.formFooter = formFooter;
+    }
+
+    public GroupData(String groupName, String formHeader, String formFooter) {
+        this.id = null;
+        this.groupName = groupName;
+        this.formHeader = formHeader;
+        this.formFooter = formFooter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(id, groupData.id) && Objects.equals(groupName, groupData.groupName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, groupName);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", groupName='" + groupName + '\'' +
+                '}';
     }
 
     public String getGroupName() {
@@ -22,4 +58,5 @@ public class GroupData {
     public String getFormFooter() {
         return formFooter;
     }
+
 }
--- addressbook-we-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-we-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -12,14 +13,20 @@ public class GroupModificationTests extends TestBase{
     public void testGroupModification(){
         app.getNavigationHelper().gotoGroupPage();
         if (! app.getGroupHelper().isGroupPresents()) {
-            app.getGroupHelper().createGroup(new GroupData("newGroup", "groupHeader", "groupFooter"));
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("modified", "test", "test"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", null, null);
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().gotoGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size(), after.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
